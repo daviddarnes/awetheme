527f42b7f347a9382bd35ec4bef5655730068397
David Darnes <me@daviddarnes.com>
2015-09-02 16:29:23 +0100

    Removes jQuery entirely, fixing #1

--- header.php
+++ header.php
@@ -7,8 +7,6 @@
 <link rel='stylesheet' type='text/css' href='<?php echo theme_url('style.css'); ?>'>
 <link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css'>
 
-<!-- jQuery -->
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
 </head>
 
 <body>
